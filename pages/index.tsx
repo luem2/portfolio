f517c4c4fc19ffd6b088aa0794ca15a6bc98ae16
Luem <luchemma@gmail.com>
2023-02-28 23:45:36 -0300

    delete logo animation

--- pages/index.tsx
+++ pages/index.tsx
@@ -56,11 +56,6 @@ export default function Home({ activeLink }) {
                         marginTop: '3rem',
                         marginBottom: '1.5rem',
                     }}
-                    animate={{
-                        scale: [1, 2, 2, 1, 1],
-                        rotate: [0, 0, 270, 270, 0],
-                        borderRadius: ['20%', '20%', '50%', '50%', '20%'],
-                    }}
                 >
                     <Image
                         src={logo2.src}
