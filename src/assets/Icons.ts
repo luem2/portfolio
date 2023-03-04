6638c00d3aa9dc51a9d162ac73a069a1bd8d86d1
Luem <luchemma@gmail.com>
2023-03-04 00:12:40 -0300

    change image readme

--- src/assets/Icons.ts
+++ src/assets/Icons.ts
@@ -5,6 +5,7 @@ export {
     AiFillInfoCircle,
     AiOutlineComment,
     AiFillHome,
+    AiFillAndroid,
 } from 'react-icons/ai'
 export {
     BsFillSunFill,
@@ -12,10 +13,12 @@ export {
     BsTelegram,
     BsFillPersonFill,
     BsStack,
+    BsFillBriefcaseFill,
 } from 'react-icons/bs'
 export { BiLinkExternal } from 'react-icons/bi'
 export { FaTelegramPlane, FaRocket } from 'react-icons/fa'
 export { GrMail } from 'react-icons/gr'
 export { MdGTranslate, MdOutlineWork, MdOutlineEmail } from 'react-icons/md'
 export { IoShareSocialSharp } from 'react-icons/io5'
+export { IoMdContact } from 'react-icons/io'
 export { VscColorMode } from 'react-icons/vsc'
